225b8772a2468d802cd381e6c34c5f87f820de04
Cyril <contact@cnicodeme.com>
2025-03-04 14:13:24 +0100

    Initial import

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# It needs python3 -m pip install --user --upgrade setuptools wheel twine
+VERSION=$(python -c "from unquotemail import __version__; print(__version__)")
+
+rm -rf unquotemail/__pycache__
+
+# First, we push to Git with the new tag version
+git add --all
+git commit -a
+git push origin master
+git tag $VERSION
+git push origin $VERSION
+
+python3 -m build
+python3 -m twine upload dist/*
+echo "Done"
